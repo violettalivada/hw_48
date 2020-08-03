cc263b8bded1b30aeda535c374e06ab46bf662e6
violettalivada <vilkalivada@gmail.com>
2020-08-03 21:35:08 +0300

    Добавлена миграция

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'amount', 'price')
+    list_display_links = ('pk', 'name')
+    list_filter = ('category',)
+    search_fields = ('name',)
+
+
+admin.site.register(Product, ProductAdmin)
