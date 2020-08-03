5de890c4fb16a08a39f0930b1f7af5461b8ce376
violettalivada <vilkalivada@gmail.com>
2020-08-03 19:50:29 +0300

    Добавлено модель товара

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+DEFAULT_CATEGORY = 'other'
+CATEGORY_CHOICES = (
+    (DEFAULT_CATEGORY, 'Разное'),
+    ('food', 'Еда'),
+    ('tech', 'Бытовая техника'),
+    ('tools', 'Инструменты'),
+    ('toys', 'Игрушки'),
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    category = models.CharField(max_length=20, default=DEFAULT_CATEGORY, choices=CATEGORY_CHOICES,
+                                verbose_name='Категория')
+    amount = models.IntegerField(verbose_name='Остаток', validators=(MinValueValidator(0),))
+    price = models.DecimalField(verbose_name='Цена', max_digits=7, decimal_places=2,
+                                validators=(MinValueValidator(0),))
+
+    def __str__(self):
+        return f'{self.name} - {self.amount}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
