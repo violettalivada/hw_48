a6ae5ff7b386cf29f4657ed54540705acb9365e3
violettalivada <vilkalivada@gmail.com>
2020-08-03 23:15:43 +0300

    Добавлены представления для главной страницы, детального просмотра, добавления, редактирования и удаления.

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_view, name='index'),
+    path('/<int:pk>/', product_view, name='product_view'),
+    path('add/', product_create_view, name='product_create'),
+    path('<int:pk>/update/', product_update_view, name='product_update'),
+    path('<int:pk>/delete/', product_delete_view, name='product_delete')
 ]
+
+
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from webapp.models import *
+from django.http import HttpResponseNotAllowed
+from webapp.forms import ProductForm
 
-# Create your views here.
+
+def index_view(request):
+    data = Product.objects.all()
+    return render(request, 'index.html', context={'products': data})
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'product_view.html', context)
+
+
+def product_create_view(request):
+    if request.method == "GET":
+        form = ProductForm()
+        return render(request, 'product_create.html', context={'form': form})
+    elif request.method == 'POST':
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            product = Product.objects.create(
+                name=form.cleaned_data['name'],
+                description=form.cleaned_data['description'],
+                category=form.cleaned_data['category'],
+                amount=form.cleaned_data['amount'],
+                price=form.cleaned_data['price'])
+            return redirect('product_view', pk=product.pk)
+        else:
+            return render(request, 'product_create.html', context={'form': form})
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
+
+
+def product_update_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        form = ProductForm(initial={
+            'name': product.name,
+            'description': product.description,
+            'category': product.category,
+            'amount': product.amount,
+            'price': product.price
+        })
+        return render(request, 'product_update.html', context={'form': form, 'product': product})
+    elif request.method == 'POST':
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            product.name = form.cleaned_data['name']
+            product.description = form.cleaned_data['description']
+            product.category = form.cleaned_data['category']
+            product.amount = form.cleaned_data['amount']
+            product.price = form.cleaned_data['price']
+            product.save()
+            return redirect('product_view', pk=product.pk)
+        else:
+            return render(request, 'product_update.html', context={'product': product, 'form': form})
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
+
+
+def product_delete_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'product_delete.html', context={'product': product})
+    elif request.method == 'POST':
+        product.delete()
+        return redirect('index')
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
